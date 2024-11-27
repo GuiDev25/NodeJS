74a83443de4f7406825a6f9753c03ce1f58a027b
Guilherme <p.galmeidafernandes@gmail.com>
2024-11-27 20:57:17 -0300

    Update app.js

--- JavaScript-básico/variaveis/app.js
+++ JavaScript-básico/variaveis/app.js
@@ -1,7 +1,21 @@
-const nome = "Pedro Guilherme";
-let idade = 21;
+//Dados de entrada
+const nome = "Teste nome";
+let idade = 20; 
 let pesoKg = 80;
-const alturaM = 1.7;
+const alturaM = 1.75;
+
+//Calc IMC
 let imc = pesoKg/(alturaM**2);
-let anoNascimento = 2024-idade;
-console.log(`${nome} tem ${idade} anos e pesa ${pesoKg}kg. Tem ${alturaM}m e seu IMC é:${imc.toFixed(2)}`);
+
+//Set Values for IMC
+if (imc < 19.1) {
+    console.log(`${nome} tem ${idade} anos e pesa ${pesoKg}kg. Tem ${alturaM}m e seu IMC é: ${imc.toFixed(2)} // IMC = Abaixo do peso`);
+} else if (imc>=19.1 && imc<=25.8) {
+    console.log(`${nome} tem ${idade} anos e pesa ${pesoKg}kg. Tem ${alturaM}m e seu IMC é: ${imc.toFixed(2)} // IMC = Normal`);
+} else if(imc>=25.9 && imc<= 27.3){
+    console.log(`${nome} tem ${idade} anos e pesa ${pesoKg}kg. Tem ${alturaM}m e seu IMC é: ${imc.toFixed(2)} // IMC = Pouco acima do peso`);
+}else if(imc>=27.4 && imc<= 32.3){
+    console.log(`${nome} tem ${idade} anos e pesa ${pesoKg}kg. Tem ${alturaM}m e seu IMC é: ${imc.toFixed(2)} // IMC = Acima do peso`);
+}else{
+    console.log(`${nome} tem ${idade} anos e pesa ${pesoKg}kg. Tem ${alturaM}m e seu IMC é: ${imc.toFixed(2)} // IMC = Obesidade`);
+}
